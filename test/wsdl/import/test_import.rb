5aa7a149432b7f0a60f94ed285d7cdcab9efe377
nahi <nahi@ruby-lang.org>
2009-01-10 14:10:17 +0000

    * test updated for [2020].

--- test/wsdl/import/test_import.rb
+++ test/wsdl/import/test_import.rb
@@ -6,27 +6,30 @@ module WSDL
 
 
 class TestImport < Test::Unit::TestCase
+  DIR = File.dirname(__FILE__)
+
   def setup
-    file = File.expand_path('import.wsdl', File.dirname(__FILE__))
+    file = File.expand_path('import.wsdl', DIR)
     @xml = File.open(file) { |f| f.read }
+    @parser = WSDL::Parser.new(:location => URI.parse("file://#{file}"))
   end
 
   def test_import
-    wsdl = WSDL::Parser.new.parse(@xml)
+    wsdl = @parser.parse(@xml)
     assert_equal('dummy', wsdl.collect_elements[0].name.name)
   end
 
   def test_file_import
-    path = File.expand_path('import.xsd', File.dirname(__FILE__))
+    path = File.expand_path('import.xsd', DIR)
     xml = @xml.sub(/import.xsd/, "file://#{path}")
-    wsdl = WSDL::Parser.new.parse(xml)
+    wsdl = @parser.parse(xml)
     assert_equal('dummy', wsdl.collect_elements[0].name.name)
   end
 
   def test_file_escaped_import # %-encode in file URI.
-    path = File.expand_path('import%2exsd', File.dirname(__FILE__))
+    path = File.expand_path('import%2exsd', DIR)
     xml = @xml.sub(/import.xsd/, "file://#{path}")
-    wsdl = WSDL::Parser.new.parse(xml)
+    wsdl = @parser.parse(xml)
     assert_equal('dummy', wsdl.collect_elements[0].name.name)
   end
 end

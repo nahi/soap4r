bcb74209d2e7cdf54ab3f191421ea4139a6015f1
nahi <nahi@ruby-lang.org>
2009-01-23 10:05:34 +0000

    * allow for correct handling of schema regex patterns.  applied patch at #475 by anonymous user.  Thanks!  closes #475.

--- lib/wsdl/xmlSchema/pattern.rb
+++ lib/wsdl/xmlSchema/pattern.rb
@@ -1,5 +1,5 @@
 # WSDL4R - XMLSchema pattern definition for WSDL.
-# Copyright (C) 2000-2007  NAKAMURA, Hiroshi <nahi@ruby-lang.org>.
+# Copyright (C) 2005-2009  NAKAMURA, Hiroshi <nahi@ruby-lang.org>.
 
 # This program is copyrighted free software by NAKAMURA, Hiroshi.  You can
 # redistribute it and/or modify it under the same terms of Ruby's license;
@@ -7,6 +7,7 @@
 
 
 require 'wsdl/info'
+require 'strscan'
 
 
 module WSDL
@@ -14,6 +15,62 @@ module XMLSchema
 
 
 class Pattern < Info
+
+  # This class and pattern translation code is copyrighted by anonymous user at
+  # ticket #475.  Thanks!
+  class CharclassTranslatorToken
+    def initialize(scanner, inside = false)
+      @container = []
+      @scanner = scanner
+      @inside = inside
+    end
+
+    def to_a
+      @container
+    end
+
+    def translate
+      while !@scanner.eos?
+        flat = @scanner.scan(/[a-zA-Z0-9]-[a-zA-Z0-9]/)
+        unless flat.nil?
+          @container << Range.new(*flat.scan(/[^-]+/)).to_a
+          next
+        end
+        c = @scanner.getch()
+        case c
+        when "["
+          c = CharclassTranslatorToken.new(@scanner,true)
+          @container << c
+          c.translate
+        when "]"  
+          break
+        else
+          @container << c
+        end    
+      end
+      if @inside 
+        i = 0
+        while i < @container.length
+          if @container[i].class != String and @container[i+1] == "-" and
+              @container[i+2].class != String 
+            @container[i] = @container[i].to_a - @container[i+2].to_a
+            @container.delete_at(i+1) 
+            @container.delete_at(i+1)
+          else
+            i += 1
+          end  
+        end  
+        @container.flatten!
+      end  
+    end
+
+    def to_s
+      @container.inject('') { |s,i|
+        i.class == String ? s << i : s << "[" + i.to_a.to_s + "]"
+      }
+    end  
+  end
+
   def initialize
     super
   end
@@ -25,8 +82,15 @@ class Pattern < Info
   def parse_attr(attr, value)
     case attr
     when ValueAttrName
+      s = value.source
+      s.gsub!("\\i",'[_:A-Za-z]')
+      s.gsub!("\\I",'[^_:A-Za-z]')
+      s.gsub!("\\c",'[-._:A-Za-z0-9]')
+      s.gsub!("\\C",'[^-._:A-Za-z0-9]')
+      ss = CharclassTranslatorToken.new(StringScanner.new(s))
+      ss.translate
       begin
-        parent.pattern = /\A#{value.source}\z/n
+        parent.pattern = /\A#{ss.to_s}\z/n
       rescue RegexpError
         warn("cannot parse as a Ruby's Regexp source (ignored): #{value.source}")
       end
